83e7d58802a5c252b65e79ece1167524d4fe323b
Gianella Carla Lastra <gianellalastra4@gmail.com>
2026-02-26 02:25:21 -0300

    Actualización de estados en interfaces de usuario con React

--- src/UseState.js
+++ src/UseState.js
@@ -1,33 +1,48 @@
 import { useEffect, useState } from "react";
 
+const SECURITY_CODE = "paradigma";
+
 function UseState({ name }) {
+    const [value, setValue] = useState("");
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    console.log(value)
     useEffect(() => {
         console.log("Empezando el efecto");
-       if (loading) {
+        if (!!loading) {
+            setError(false);
             setTimeout(() => {
                 console.log("Haciendo la validación");
+                if (value !== SECURITY_CODE) {
+                    setError(true);
+                }
                 setLoading(false);
+
+
                 console.log("Terminando la validación");
             }, 3000);
-        } 
+        }
         console.log("Terminando el efecto");
-    }, [loading])
+    }, [loading, value]);
+
     return (
         <div>
             <h2>Eliminar {name}</h2>
 
             <p>Por favor, escribe el codigo de seguridad.</p>
 
-            {error && 
-            (<p>Error: el código es incorrecto</p>)}
+            {(error && !loading) &&
+                (<p>Error: el código es incorrecto</p>)}
 
-            {loading && 
-            (<p>cargando...</p>)}
+            {loading &&
+                (<p>cargando...</p>)}
 
-            <input placeholder="Código de seguridad" />
+            <input
+                placeholder="Código de seguridad"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
             <button onClick={() => setLoading(!loading)}>
                 Comprobar
             </button>
